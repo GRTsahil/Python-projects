7373ff5e4f255fef22c748ae58079795daa931d4
GRTsahil <42871725+GRTsahil@users.noreply.github.com>
2020-12-20 15:49:30 +0530

    Add files via upload

---
+++ background_MAGIC_CLOAK.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+cap=cv2.VideoCapture(0)
+
+while cap.isOpened():
+	ret, back = cap.read()
+	if ret:
+		cv2.imshow("image",back)
+		if cv2.waitKey(3)==ord("q"):
+			cv2.imwrite('image.jpg',back)
+			break
+cap.release()
+cv2.destroyAllWindows()
